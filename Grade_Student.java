b4c09df79e5eec0ac5d7988c3f3726dacd73e6d7
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-10 20:55:55 +0530

    Add files via upload

---
+++ Grade_Student.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Grade_Student{
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int mark=sc.nextInt();
+        
+        if(mark>90)
+        {
+            System.out.println("excellent");
+        }
+        else if(mark>80)
+        {
+            System.out.println("good");
+        }
+        else if(mark>70)
+        {
+            System.out.println("fair");
+        }
+        else if(mark>60)
+        {
+            System.out.println("meet expectations");
+        }
+        else if(mark>40)
+        {
+            System.out.println("below par");
+        }
+        else
+        {
+            System.out.println("failed");
+        }
+    }
+}
