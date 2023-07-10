2fd1710ad032fd1372e44827027725a3b86e9e64
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-10 20:55:35 +0530

    Add files via upload

---
+++ Greater_number.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Greater_number{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+        if(a>b)
+        {
+            System.out.println(a);
+        }
+        else
+        {
+            System.out.println(b);
+        }
+    }
+}
