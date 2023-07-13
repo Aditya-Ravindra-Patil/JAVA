5d3a6870187206523ce3cc9d3b5fbf642e8535a6
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-13 23:34:03 +0530

    Add files via upload

---
+++ Ternary_Operator.java
@@ -0,0 +1,23 @@
+/*
+																NOTES													11-07-2023
+
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Ternary_Operator{
+
+    public static void main(String[] args) {
+        
+		int a='a';
+		int b=3;
+		
+		System.out.println(((a%b)==0)? "Condition True\n"+a : "Condition is Flase\n"+b);
+		
+		System.out.println(((a%b)==1)? "Condition True\n"+a : "Condition is Flase\n"+b);
+		
+
+		
+    }
+}
