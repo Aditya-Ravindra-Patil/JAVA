581efbb79c3555e83264ec92fbe18016f892fb56
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-13 23:31:33 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,24 @@
+/*
+																NOTES													10-07-2023
+Addition(+):
+The operater which is to perform Addition operation on the operands such a operator is called as Addition operator.
+
+
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Add{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=2147483647;
+        int b=2;
+        System.out.println(a+b);
+		char c='a';
+		char d=c+3;
+		System.out.println(d);
+    }
+}
