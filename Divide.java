6598e190af242c2db6be6b49f076ac7fe48d9eeb
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-13 23:32:22 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,22 @@
+/*
+																NOTES													10-07-2023
+Division(/):
+The operater which is to perform Division operation on the operands such a operator is called as Division operator.
+
+
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Divide{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=21;
+        int b=2;
+        System.out.println(a/b);
+		
+    }
+}
