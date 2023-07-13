0e636bf8d13f3c21d4c5fb91472f4078359d1fe7
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-13 23:33:16 +0530

    Add files via upload

---
+++ Relational_Operator.java
@@ -0,0 +1,31 @@
+/*
+																NOTES													10-07-2023
+Relational Operator: It is used to compare 2 variable or 2 datas.
+
+type of relational operator:
+< ------less than
+<= -----less than equal too
+> ------greater than
+>= -----greater than equal too
+== -----equal to equal too
+!= -----not equal too
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Relational_Operator{
+
+    public static void main(String[] args) {
+        
+		char x='s';
+		char y='m';
+		System.out.println(x<y);
+		System.out.println(x<=y);
+		System.out.println(x>y);
+		System.out.println(x>=y);
+		System.out.println(x==y);
+		System.out.println(x!=y);
+		
+    }
+}
