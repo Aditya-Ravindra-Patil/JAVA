da958554b57fd5f9c696350e986a5da9910af19f
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-14 23:54:52 +0530

    Add files via upload

---
+++ Reverse_Table.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Reverse_Table{
+
+    public static void main(String[] args) {
+
+        int a=5;
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(a+"x"+i+"="+a*i);
+        }
+    }
+}
