61a887543669f80533719a99432a4168c6a8726d
Aditya Patil <101046848+Aditya-Ravindra-Patil@users.noreply.github.com>
2023-07-14 23:52:39 +0530

    Add files via upload

---
+++ Print_a_to_z.java
@@ -0,0 +1,18 @@
+/*
+You have to print characters from a till z where each character should be printed in a separate line.
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Print_a_to_z{
+
+    public static void main(String[] args) {
+
+        for(char i='a';i<='z';i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
